9a74e174969d9b49fe0cae4c4aa3adf6c1bc86a1
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2025-07-30 08:45:53 +0000

    add more tests

--- p256_hashtocurve_test.go
+++ p256_hashtocurve_test.go
@@ -3,6 +3,7 @@
 package nistec_test
 
 import (
+	"encoding/hex"
 	"fmt"
 	"testing"
 
@@ -10,20 +11,75 @@ import (
 )
 
 func TestMapToCurve(t *testing.T) {
-	ubytes := []byte{0xad, 0x53, 0x42, 0xc6, 0x6a, 0x6d, 0xd0, 0xff, 0x08, 0x0d, 0xf1, 0xda, 0x0e, 0xa1, 0xc0, 0x4b, 0x96, 0xe0, 0x33, 0x0d, 0xd8, 0x94, 0x06, 0x46, 0x5e, 0xeb, 0xa1, 0x15, 0x82, 0x51, 0x50, 0x09}
-	//ubytes = []byte{0x8c, 0x0f, 0x1d, 0x43, 0x20, 0x4b, 0xd6, 0xf6, 0xea, 0x70, 0xae, 0x80, 0x13, 0x07, 0x0a, 0x15, 0x18, 0xb4, 0x38, 0x73, 0xbc, 0xd8, 0x50, 0xaa, 0xfa, 0x0a, 0x9e, 0x22, 0x0e, 0x2e, 0xea, 0x5a}
-	p, err := nistec.P256MapToCurve(ubytes)
-	if err != nil {
-		t.Errorf("unexpected error: %v", err)
-		return
-	}
-	wantX := "ab640a12220d3ff283510ff3f4b1953d09fad35795140b1c5d64f313967934d5"
-	wantY := "dccb558863804a881d4fff3455716c836cef230e5209594ddd33d85c565b19b1"
-	x, y := nistec.GetX(p), nistec.GetY(p)
-	if fmt.Sprintf("%x", x) != wantX {
-		t.Errorf("x = %x, want %s", x, wantX)
-	}
-	if fmt.Sprintf("%x", y) != wantY {
-		t.Errorf("x = %x, want %s", x, wantY)
+	for _, tt := range []struct {
+		u, x, y string
+	}{
+		{
+			u: "ad5342c66a6dd0ff080df1da0ea1c04b96e0330dd89406465eeba11582515009",
+			x: "ab640a12220d3ff283510ff3f4b1953d09fad35795140b1c5d64f313967934d5",
+			y: "dccb558863804a881d4fff3455716c836cef230e5209594ddd33d85c565b19b1",
+		},
+		{
+			u: "8c0f1d43204bd6f6ea70ae8013070a1518b43873bcd850aafa0a9e220e2eea5a",
+			x: "51cce63c50d972a6e51c61334f0f4875c9ac1cd2d3238412f84e31da7d980ef5",
+			y: "b45d1a36d00ad90e5ec7840a60a4de411917fbe7c82c3949a6e699e5a1b66aac",
+		},
+		{
+			u: "afe47f2ea2b10465cc26ac403194dfb68b7f5ee865cda61e9f3e07a537220af1",
+			x: "5219ad0ddef3cc49b714145e91b2f7de6ce0a7a7dc7406c7726c7e373c58cb48",
+			y: "7950144e52d30acbec7b624c203b1996c99617d0b61c2442354301b191d93ecf",
+		},
+		{
+			u: "379a27833b0bfe6f7bdca08e1e83c760bf9a338ab335542704edcd69ce9e46e0",
+			x: "019b7cb4efcfeaf39f738fe638e31d375ad6837f58a852d032ff60c69ee3875f",
+			y: "589a62d2b22357fed5449bc38065b760095ebe6aeac84b01156ee4252715446e",
+		},
+		{
+			u: "0fad9d125a9477d55cf9357105b0eb3a5c4259809bf87180aa01d651f53d312c",
+			x: "a17bdf2965eb88074bc01157e644ed409dac97cfcf0c61c998ed0fa45e79e4a2",
+			y: "4f1bc80c70d411a3cc1d67aeae6e726f0f311639fee560c7f5a664554e3c9c2e",
+		},
+		{
+			u: "b68597377392cd3419d8fcc7d7660948c8403b19ea78bbca4b133c9d2196c0fb",
+			x: "7da48bb67225c1a17d452c983798113f47e438e4202219dd0715f8419b274d66",
+			y: "b765696b2913e36db3016c47edb99e24b1da30e761a8a3215dc0ec4d8f96e6f9",
+		},
+		{
+			u: "3bbc30446f39a7befad080f4d5f32ed116b9534626993d2cc5033f6f8d805919",
+			x: "c76aaa823aeadeb3f356909cb08f97eee46ecb157c1f56699b5efebddf0e6398",
+			y: "776a6f45f528a0e8d289a4be12c4fab80762386ec644abf2bffb9b627e4352b1",
+		},
+		{
+			u: "76bb02db019ca9d3c1e02f0c17f8baf617bbdae5c393a81d9ce11e3be1bf1d33",
+			x: "418ac3d85a5ccc4ea8dec14f750a3a9ec8b85176c95a7022f391826794eb5a75",
+			y: "fd6604f69e9d9d2b74b072d14ea13050db72c932815523305cb9e807cc900aff",
+		},
+		{
+			u: "4ebc95a6e839b1ae3c63b847798e85cb3c12d3817ec6ebc10af6ee51adb29fec",
+			x: "d88b989ee9d1295df413d4456c5c850b8b2fb0f5402cc5c4c7e815412e926db8",
+			y: "bb4a1edeff506cf16def96afff41b16fc74f6dbd55c2210e5b8f011ba32f4f40",
+		},
+		{
+			u: "4e21af88e22ea80156aff790750121035b3eefaa96b425a8716e0d20b4e269ee",
+			x: "a281e34e628f3a4d2a53fa87ff973537d68ad4fbc28d3be5e8d9f6a2571c5a4b",
+			y: "f6ed88a7aab56a488100e6f1174fa9810b47db13e86be999644922961206e184",
+		},
+	} {
+		ubytes, err := hex.DecodeString(tt.u)
+		if err != nil {
+			panic(err)
+		}
+		p, err := nistec.P256MapToCurve(ubytes)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+			return
+		}
+		x, y := nistec.GetX(p), nistec.GetY(p)
+		if fmt.Sprintf("%x", x) != tt.x {
+			t.Errorf("u = %s: got x = %x, want %s", tt.u, x, tt.x)
+		}
+		if fmt.Sprintf("%x", y) != tt.y {
+			t.Errorf("u = %s: got x = %x, want %s", tt.u, y, tt.y)
+		}
 	}
 }
